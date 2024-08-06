fc047837fd91ddf0ec74df65c45f0c6891741943
khausbak <py.hauken@gmail.com>
2024-08-06 05:21:23 +0200

    base structure
    installed webpack, initial setup with skeleton html, css and js files. Added basic content to ensure everything is connected

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './css-files/landingpage.css';
+
+const testFunc = function() {
+    console.log("Hello, world");
+    const titleDiv = document.querySelector("#title-div");
+    docObj.textContent = "This is a test";
+}
+
+testFunc();
